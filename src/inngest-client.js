a15bd3f51cb36247ec932e1f8065ad09de715f0e
Chad Morgan <chad@creativepartnersolutions.com>
2026-03-07 20:00:31 +0000

    Add src/inngest-client.js

---
+++ src/inngest-client.js
@@ -0,0 +1,6 @@
+import { Inngest } from 'inngest';
+
+export const inngest = new Inngest({ 
+  id: 'creative-partner-os',
+  // Signing key is picked up automatically from INNGEST_SIGNING_KEY env var
+});
